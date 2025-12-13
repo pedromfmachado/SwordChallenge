ba801ba89c2e2dd94cfc011753247724ffe58fb5
Pedro Machado <pedromfmachado@gmail.com>
2025-12-13 16:10:32 +0000

    feat(cat_breeds): update ListScreen to handle loading and error states

--- cat_breeds/src/main/java/com/pedromfmachado/sword/catz/catbreeds/presentation/ui/ListScreen.kt
+++ cat_breeds/src/main/java/com/pedromfmachado/sword/catz/catbreeds/presentation/ui/ListScreen.kt
@@ -1,10 +1,20 @@
 package com.pedromfmachado.sword.catz.catbreeds.presentation.ui
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.compose.ui.res.stringResource
+import androidx.hilt.navigation.compose.hiltViewModel
+import com.pedromfmachado.sword.catz.catbreeds.R
 import com.pedromfmachado.sword.catz.catbreeds.domain.model.Breed
 import com.pedromfmachado.sword.catz.catbreeds.presentation.ui.components.breed.BreedList
+import com.pedromfmachado.sword.catz.catbreeds.presentation.viewmodel.BreedListUiState
 import com.pedromfmachado.sword.catz.catbreeds.presentation.viewmodel.BreedListViewModel
 
 @Composable
@@ -13,10 +23,32 @@ fun ListScreen(
     modifier: Modifier = Modifier,
     viewModel: BreedListViewModel = hiltViewModel()
 ) {
-    BreedList(
-        breeds = viewModel.breeds,
-        onBreedClick = onBreedClick,
-        onFavoriteClick = { /* No action for now */ },
-        modifier = modifier
-    )
+    val uiState by viewModel.uiState.collectAsState()
+
+    when (val state = uiState) {
+        is BreedListUiState.Loading -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                CircularProgressIndicator()
+            }
+        }
+        is BreedListUiState.Success -> {
+            BreedList(
+                breeds = state.breeds,
+                onBreedClick = onBreedClick,
+                onFavoriteClick = { /* No action for now */ },
+                modifier = modifier
+            )
+        }
+        is BreedListUiState.Error -> {
+            Box(
+                modifier = modifier.fillMaxSize(),
+                contentAlignment = Alignment.Center
+            ) {
+                Text(text = state.message ?: stringResource(R.string.screen_list_error_generic))
+            }
+        }
+    }
 }
