50446d5c7a9e0bcb450522a09ac52fb7736667c7
Pedro Machado <pedromfmachado@gmail.com>
2025-12-13 15:57:39 +0000

    feat(data): add NetworkModule for Retrofit DI

---
+++ cat_breeds_data/src/main/java/com/pedromfmachado/sword/catz/catbreeds/data/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package com.pedromfmachado.sword.catz.catbreeds.data.di
+
+import com.pedromfmachado.sword.catz.catbreeds.data.BuildConfig
+import com.pedromfmachado.sword.catz.catbreeds.data.api.CatApiService
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    private const val BASE_URL = "https://api.thecatapi.com/v1/"
+
+    @Provides
+    @Singleton
+    fun provideOkHttpClient(): OkHttpClient =
+        OkHttpClient.Builder()
+            .addInterceptor { chain ->
+                val request = chain.request().newBuilder()
+                    .addHeader("x-api-key", BuildConfig.CAT_API_KEY)
+                    .build()
+                chain.proceed(request)
+            }
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideMoshi(): Moshi =
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideRetrofit(okHttpClient: OkHttpClient, moshi: Moshi): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .build()
+
+    @Provides
+    @Singleton
+    fun provideCatApiService(retrofit: Retrofit): CatApiService =
+        retrofit.create(CatApiService::class.java)
+}
