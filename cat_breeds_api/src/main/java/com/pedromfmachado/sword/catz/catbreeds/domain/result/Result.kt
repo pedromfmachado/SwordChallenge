0392b4f32cdeecd0f3f93320cc14413cc7892b47
Pedro Machado <pedromfmachado@gmail.com>
2025-12-13 15:46:02 +0000

    feat(api): add Result sealed class for error handling

---
+++ cat_breeds_api/src/main/java/com/pedromfmachado/sword/catz/catbreeds/domain/result/Result.kt
@@ -0,0 +1,6 @@
+package com.pedromfmachado.sword.catz.catbreeds.domain.result
+
+sealed class Result<out T> {
+    data class Success<T>(val data: T) : Result<T>()
+    data class Error(val exception: Throwable) : Result<Nothing>()
+}
